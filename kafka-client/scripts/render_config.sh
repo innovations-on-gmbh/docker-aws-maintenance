533e0a4f09e6cf8a009649e8a15610800055ea42
Andreas Kumlehn <andreas@kumlehn.de>
2023-03-05 19:44:53 +0100

    fix: kafka launch command and variable name, bump version

--- kafka-client/scripts/render_config.sh
+++ kafka-client/scripts/render_config.sh
@@ -6,5 +6,5 @@ security.protocol=SASL_SSL
 sasl.mechanism=SCRAM-SHA-512
 sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule required \
   username='config-admin' \
-  password='$CONFIG_USER';
+  password='$PASSWORD';
 EOT
