9469096fc45a330c762cc4328a84163ec3b12d40
Andreas Kumlehn <andreas@kumlehn.de>
2023-03-09 15:55:33 +0100

    fix: env var

--- kafka-client/launch_kafka_client.sh
+++ kafka-client/launch_kafka_client.sh
@@ -5,7 +5,7 @@ set -eu
 
 kubectl run --rm -ti -n default kafka-client \
   --image=ghcr.io/innovations-on-gmbh/docker-aws-maintenance/kafka-client:latest \
-  --env="CONFIG_USER=$(sops -d --extract '["kafka"]["config_user"]["password"]' credentials.yaml)" \
+  --env="PASSWORD=$(sops -d --extract '["kafka"]["config_user"]["password"]' credentials.yaml)" \
   --env="BOOTSTRAP=$(aws kafka list-clusters-v2 --output text --query 'ClusterInfoList[*].ClusterArn' | xargs -I {} aws kafka get-bootstrap-brokers --cluster-arn {} --output text)" \
   --env="CLUSTER_ARN=$(aws kafka list-clusters-v2 --output text --query 'ClusterInfoList[*].ClusterArn')" \
   --image-pull-policy=Always --overrides='{"metadata": {"annotations": { "linkerd.io/inject":"disabled" } } }' \
