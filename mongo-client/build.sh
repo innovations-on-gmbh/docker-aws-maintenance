d3181059f5c7790102af7db06ad7a12aebadb05b
Andreas Kumlehn <andreas@kumlehn.de>
2023-03-31 12:31:55 +0200

    mongo-client: bump tag

--- mongo-client/build.sh
+++ mongo-client/build.sh
@@ -2,7 +2,7 @@
 
 set -exu
 
-TAG=0.3
+TAG=0.4
 REPO=ghcr.io/innovations-on-gmbh/docker-aws-maintenance/mongo-client
 
 docker build --platform=linux/amd64 --tag "$REPO":"$TAG" --tag "$REPO":latest .
