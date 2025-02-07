675c150e6a26fbc7ef36ca3667eee272c43deab7
Andreas Kumlehn <andreas@kumlehn.de>
2025-02-07 16:05:32 +0100

    feat: add tls advertised listeners, extend readme

--- kafka-client/README.md
+++ kafka-client/README.md
@@ -3,3 +3,8 @@
 ```zsh
 kubectl run --rm -ti -n default kafka-client --image=ghcr.io/innovations-on-gmbh/docker-aws-maintenance/kafka-client:0.1 --env="CONFIG_USER=$(sops -d --extract '["kafka"]["config_user"]["password"]' credentials.yaml)" --env="BOOTSTRAP=$(aws kafka list-clusters-v2 --output text --query 'ClusterInfoList[*].ClusterArn' | xargs -I {} aws kafka get-bootstrap-brokers --cluster-arn {} --output text)"
 ```
+
+```zsh
+export DNS_ZONE=test.de
+kafka-configs.sh --bootstrap-server "b-1.$DNS_ZONE:9096" --entity-type brokers --entity-name 1 --command-config /config.properties --all --describe
+```
--- kafka-client/scripts/update_advertised_listeners.sh
+++ kafka-client/scripts/update_advertised_listeners.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-DNS_ZONE=prodonpierkafka.sifyfu.c6.kafka.eu-central-1.amazonaws.com
+DNS_ZONE=devonpier-kafka.gereae.c1.kafka.eu-central-1.amazonaws.com
 
 for i in {1..3}; do
     until kafka-configs.sh \
@@ -10,7 +10,7 @@ for i in {1..3}; do
         --entity-name "$i" \
         --alter \
         --command-config /config.properties \
-        --add-config advertised.listeners=[CLIENT_SASL_SCRAM://b-"$i".$DNS_ZONE:900"$i",REPLICATION://b-"$i"-internal.$DNS_ZONE:9093,REPLICATION_SECURE://b-"$i"-internal.$DNS_ZONE:9095]
+        --add-config advertised.listeners=[CLIENT_SECURE://b-"$i".$DNS_ZONE:901"$i",CLIENT_SASL_SCRAM://b-"$i".$DNS_ZONE:900"$i",REPLICATION://b-"$i"-internal.$DNS_ZONE:9093,REPLICATION_SECURE://b-"$i"-internal.$DNS_ZONE:9095]
     do
         echo "retrying"
     done
