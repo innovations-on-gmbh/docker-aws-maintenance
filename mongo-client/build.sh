f763ad1ed9bd38f77fcef82e58b31a56bcead96b
Andreas Kumlehn <andreas@kumlehn.de>
2025-07-09 11:07:26 +0200

    bump: mongo major version to 8, use latest aws private certificate

--- mongo-client/build.sh
+++ mongo-client/build.sh
@@ -2,7 +2,7 @@
 
 set -exu
 
-TAG=0.4
+TAG=0.5
 REPO=ghcr.io/innovations-on-gmbh/docker-aws-maintenance/mongo-client
 
 docker build --platform=linux/amd64 --tag "$REPO":"$TAG" --tag "$REPO":latest .
--- mongo-client/scripts/connect.sh
+++ mongo-client/scripts/connect.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-mongosh mongodb://"$MASTER_USERNAME":"$PASSWORD"@"$ENDPOINT":27017/?ssl=true\&retryWrites=false --tlsCAFile rds-combined-ca-bundle.pem --tls
+mongosh mongodb://"$MASTER_USERNAME":"$PASSWORD"@"$ENDPOINT":27017/?ssl=true\&retryWrites=false --tlsCAFile global-bundle.pem --tls
